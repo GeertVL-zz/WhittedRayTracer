7718b5426506af0b6553ebdac86888297cbfe048
Geert Van Laethem <geert.van.laethem@telenet.be>
2018-10-27 17:37:56 +0200

    Initial Commit

---
+++ WhittedRayTracer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhittedRayTracer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
