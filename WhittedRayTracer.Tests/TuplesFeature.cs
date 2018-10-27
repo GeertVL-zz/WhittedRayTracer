2fb33f423525ebab4e412495056c5b349bf7ce06
Geert Van Laethem <geert.van.laethem@telenet.be>
2018-10-27 17:54:32 +0200

    Is a Point or a vector

---
+++ WhittedRayTracer.Tests/TuplesFeature.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace WhittedRayTracer.Tests
+{
+    public class TuplesFeature
+    {
+        [Fact(DisplayName = "A tuple with w=1.0 is a point")]        
+        public void ATupleWithW1IsAPoint()
+        {
+            var a = (x: 4.3, y: -4.2, z: 3.1, w: 1.0);
+            
+            Assert.Equal(4.3, a.x);
+            Assert.Equal(-4.2, a.y);
+            Assert.Equal(3.1, a.z);
+            Assert.Equal(1.0, a.w);
+            
+            Assert.True(a.IsAPoint());
+            Assert.False(a.IsAVector());
+        }
+    }
+}
---
+++ WhittedRayTracer/TupleExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace WhittedRayTracer
+{
+    public static class TupleExtensions
+    {
+        public static bool IsAPoint(this (double x, double y, double z, double w) tuple)
+        {
+            return tuple.w == 1.0;
+        }
+
+        public static bool IsAVector(this (double x, double y, double z, double w) tuple)
+        {
+            return tuple.w == 0;
+        }
+    }
+}
